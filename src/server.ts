84646576e2cd5ad9212514068ffb29bf0b9d433e
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-11-10 09:24:12 -0300

    feat: add new upload route

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,25 @@
+import { fastifyCors } from "@fastify/cors";
+import {fastifyMultipart} from '@fastify/multipart'
 import { fastify } from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
-import { fastifyCors } from "@fastify/cors";
 import { env } from "./env.ts";
-import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+
 import { createRoomRoute } from "./http/routes/create-room.ts";
-import { getRoomQuestions } from "./http/routes/get-room-questions.ts";
+import { getRoomQuestionsRoute } from "./http/routes/get-room-questions.ts";
+import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+import { uploadAudioRoute } from "./http/routes/upload-audio.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
   origin: "http://localhost:5173",
 });
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
@@ -25,7 +30,8 @@ app.get("/health", () => {
 
 app.register(getRoomsRoute)
 app.register(createRoomRoute)
-app.register(getRoomQuestions)
+app.register(getRoomQuestionsRoute)
+app.register(uploadAudioRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log("HTTP server running!");
